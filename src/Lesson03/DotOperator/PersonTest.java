0cbe27c766d2be6ec7be660be9c4212ff0363492
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-02-04 14:30:18 +0100

    TriangelTest-opgave (første exercise) løst. Simpelt, men det virker...

--- src/Lesson03/DotOperator/PersonTest.java
+++ src/Lesson03/DotOperator/PersonTest.java
@@ -7,13 +7,21 @@ public class PersonTest {
         String fName = "Andreas";
         String mName = "Elias";
         String lName = "Mogensen";
+        String nationality = "Danmark";
         int age = 47;
 
-        OpretNytPersonObjekt(fName, mName, lName, age);
+        String fName2 = "Michael";
+        String mName2 = "B.";
+        String lName2 = "Jordan";
+        String nationality2 = "USA";
+        int age2 = 38;
+
+        OpretNytPersonObjekt(fName, mName, lName, age, nationality);
+        OpretNytPersonObjekt(fName2, mName2, lName2, age2, nationality2);
 
     }
 
-    public static void OpretNytPersonObjekt(String fName, String mName, String lName, int age)
+    public static void OpretNytPersonObjekt(String fName, String mName, String lName, int age, String nationality)
     {
 
         // Opretter et nyt Person-objekt
@@ -23,10 +31,15 @@ public class PersonTest {
         p.firstName = fName;
         p.middleName = mName;
         p.lastName = lName;
+        p.nationality = nationality;
         p.age = age;
 
         //Udskriv
         p.WritePersonInfo();
+
     }
+
+
+
 }
 
--- src/Lesson04/Costructor/Triangle/TriangleTest.java
+++ src/Lesson04/Costructor/Triangle/TriangleTest.java
@@ -2,12 +2,28 @@ package Lesson04.Costructor.Triangle;
 
 public class TriangleTest {
     public static void main(String[] args) {
+
         Triangle t1 = new Triangle(3, 4, 5);
         t1.showInfo();
 
-        System.out.println();
-
         Triangle t2 = new Triangle(6, 6, 6);
         t2.showInfo();
+
+        Triangle t3 = new Triangle(8, 9, 10);
+        t3.showInfo();
+
+        double area1 = t1.getArea();
+        double area2 = t2.getArea();
+        double area3 = t3.getArea();
+
+        System.out.println("\nResultat: ");
+
+        if (area1 > area2 && area1 > area3) {
+            System.out.println("Triangel 1 er størst.");
+        } else if (area2 > area1 && area2 > area3) {
+            System.out.println("Triangel 2 er størst.");
+        } else if (area3 > area1 && area3 > area2) {
+            System.out.println("Triangel 3 er størst.");
+        }
     }
 }
