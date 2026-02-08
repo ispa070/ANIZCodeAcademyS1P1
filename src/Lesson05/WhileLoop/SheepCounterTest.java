fcea77c7b2e2f3329bb0b02832a640462a807385
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-02-08 23:41:31 +0100

    SheepCounter øvelse, der virker.

--- src/Lesson05/WhileLoop/SheepCounterTest.java
+++ src/Lesson05/WhileLoop/SheepCounterTest.java
@@ -1,8 +1,19 @@
 package Lesson05.WhileLoop;
+import java.util.Scanner;
 
 public class SheepCounterTest {
     public static void main(String[] args) {
-        SheepCounter counter = new SheepCounter(5);
+
+        System.out.println("How many sheep would you like to count down from?");
+        Scanner scanner = new Scanner(System.in);
+        int maxInput = scanner.nextInt();
+
+        SheepCounter counter = new SheepCounter(maxInput);
         counter.countSheep();
-    }
+
+
+
+
+}
+
 }
