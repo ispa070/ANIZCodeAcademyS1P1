0e2f08e3f0e12fbd27a803f69fd31305e567a6c1
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-02-12 09:38:56 +0100

    Quick save

--- src/Lesson07/Array/MiniPojects/Flower/FlowerShopMain.java
+++ src/Lesson07/Array/MiniPojects/Flower/FlowerShopMain.java
@@ -6,13 +6,13 @@ public class FlowerShopMain {
 
     public static void main(String[] args) {
 
-        Flower items = new Flower();//du stoppede her
-
         //flower array
         Flower[] flowerItem = new Flower[3];
         flowerItem[0] = new Flower("Roser", "Røde", 79.99);
         flowerItem[1] = new Flower("Tulipaner", "Gule", 29.99);
         flowerItem[2] = new Flower("Liljer", "Hvide", 59.99);
+
+        System.out.println("Her printer vi via for loop: ");
 
         //print alle flowerItem fra Flower[]
         for (int i = 0; i < flowerItem.length; i++) {
@@ -23,13 +23,20 @@ public class FlowerShopMain {
 
         //print tomt linje mellemrum
         System.out.println();
+        System.out.println("Her er navnene på de forskellige typer blomster, vi tilbyder: ");
 
-        //for loop, der "traverser" flowerItem i vores Flower[] array
-        for (Flower item : flowerItem) {
-            System.out.println(item);
+        //enhanced for loop, der søger efter farver i flowerItem i vores Flower[] array
+        for (Flower f : flowerItem) {
+            System.out.println(f.getName());
         }
 
-        displayItemCategory();
+        //print
         System.out.println();
+        System.out.println("Her er alle de farver blomster, vi tilbyder: ");
+
+        //enhanced for loop, der søger efter farver i flowerItem i vores Flower[] array
+        for (Flower f : flowerItem) {
+            System.out.println(f.getColor());
+        }
     }
 }
