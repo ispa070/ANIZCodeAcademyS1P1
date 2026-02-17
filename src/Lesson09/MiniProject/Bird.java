38035259aef5094caa40fe73f8d9fe8da7c5ab1d
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-02-17 12:00:59 +0100

    PetAdoptionCenter update 2

--- src/Lesson09/MiniProject/Bird.java
+++ src/Lesson09/MiniProject/Bird.java
@@ -7,13 +7,22 @@ package Lesson09.MiniProject;
 public class Bird extends Pet {
     private double wingSpan;
 
-    public Bird(String name) {
+    public Bird(String name, double wingSpan) {
         super(name); // Call constructor of Pet
         this.wingSpan = wingSpan;
     }
 
+    public double getWingSpan() {
+        return wingSpan;
+    }
+
+    public void setWingSpan(double wingSpan) {
+        this.wingSpan = wingSpan;
+    }
+
+
     @Override
     public String toString() {
-        return "Bird: " + getName() + " (chirp, chirp!)";
+        return "Bird: " + getName() + "| The wingspan of "+ getName()+" is " + wingSpan +" cm. " + getName() +" seems happy about that and does a little: Chirp, chirp!";
     }
 }
